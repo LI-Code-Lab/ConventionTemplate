af84d842416aedb451d3a24fb4b994c207c3badd
Brian Roper <broper138@gmail.com>
2018-01-08 13:11:43 -0500

    initial commit

---
+++ ConventionTemplate/ConventionTemplatePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ConventionTemplate.Models;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System;
+
+namespace ConventionTemplate
+{
+    public partial class ConventionTemplatePage : ContentPage
+    {
+        public ConventionTemplatePage()
+        {
+            InitializeComponent();
+
+            //PanelList.ItemsSource = new List<PanelGuest> {
+            //    new PanelGuest{name="Henry Cavil", description="ahgsfdhjkagsGVDJKHasgvdjhgaSJKHDG", imageUrl="ConventionTemplate.Images.background.jpg", notes="ajhsgdjhasdbfjabfd"},
+            //    new PanelGuest{name="Henry Cavil", description="ahgsfdhjkagsGVDJKHasgvdjhgaSJKHDG", imageUrl="https://unsplash.com/photos/b8rkmfxZjdU", notes="ajhsgdjhasdbfjabfd"},
+            //    new PanelGuest{name="Henry Cavil", description="ahgsfdhjkagsGVDJKHasgvdjhgaSJKHDG", imageUrl="https://unsplash.com/photos/b8rkmfxZjdU", notes="ajhsgdjhasdbfjabfd"}
+            //};
+
+            var imageSource = new UriImageSource { Uri = new Uri("https://wallpaperbrowse.com/media/images/4084331-simple-images.jpg") };
+
+            var webImage = new Image { Source = ImageSource.FromUri(new Uri("https://xamarin.com/content/images/pages/forms/example-app.png")) };
+
+
+            image.Source = imageSource;
+        }
+    }
+}
---
+++ ConventionTemplate/Models/PanelGuest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ConventionTemplate.Models
+{
+    public class PanelGuest
+    {
+        public string name { get; set; }
+        public string imageUrl { get; set; }
+        public string description { get; set; }
+        public string notes { get; set; }
+    }
+}
